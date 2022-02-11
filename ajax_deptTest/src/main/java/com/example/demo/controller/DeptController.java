a6f4a32420b6fc29ec7cd3c3966bbc8ee18d14b6
khs17 <khs17@DESKTOP-CGMNMTI>
2022-02-11 20:09:06 +0900

    day0211[ajax통신]

---
+++ ajax_deptTest/src/main/java/com/example/demo/controller/DeptController.java
@@ -0,0 +1,61 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.dao.DeptDAO;
+ 
+import com.example.demo.vo.DeptVO;
+
+@Controller
+public class DeptController {
+	
+	@Autowired
+	private DeptDAO dao;
+		
+	public void setDao(DeptDAO dao) {
+		this.dao = dao;
+	}
+
+	//ajvax통신으로 요청되었을 때 jsp로 이동하지 않고, 부서목록을 josn으로 응답하도록 하기
+	//스프링부트에서는 내가 직접 json으로 변환하지 앟아도 List로 반환하면 알아서 json으로 만들어줌 
+	@ResponseBody
+	@RequestMapping("/listDept.do")
+	public List<DeptVO> list() {	
+		return dao.findAll();
+	}
+	
+	@RequestMapping(value = "/insertDept.do", method = RequestMethod.GET)
+	public void insertForm() {		
+	}
+	
+	@RequestMapping(value = "/insertDept.do", method = RequestMethod.POST)
+	public ModelAndView insertSubmit(DeptVO d) {
+		ModelAndView mav = new ModelAndView("redirect:/listDept.do");
+		int re= dao.insert(d);
+		if(re != 1) {
+			mav.setViewName("error");
+			mav.addObject("msg", "부서등록에 실패파였습니다.");
+		}
+		return mav;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
